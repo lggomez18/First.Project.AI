871032a8457aa9a3484f8e3c4cfc93ff810554c6
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-04-20 12:06:57 -0500

    ApiUrl text again
    Missing characters (commas and periods, etc) affected the response of the console log.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let apiKey = "290724cd93ad94b31t54c30cca2o800f";
 let context ="Provide specific information";
-let prompt = "When was AI first created?"
+let prompt = "When was AI first created?";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-console.log (apiUrl);
+console.log(apiUrl);
