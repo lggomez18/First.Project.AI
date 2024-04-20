8e01d55c918cded27ae207255ea6270062549974
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-04-20 12:31:28 -0500

    adding functions - axios

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+function showAnswer(response) {
+    console.log(response.data.answer);
+
+}
+
 let apiKey = "290724cd93ad94b31t54c30cca2o800f";
 let context ="Provide specific information";
 let prompt = "When was AI first created?";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+console.log("processing");
 axios.get(apiUrl).then(showAnswer)
