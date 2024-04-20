8997a194452159736b34fd7d28b49f6bf602b97d
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-04-20 12:57:10 -0500

    Practicing in progress

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function showAnswer(response) {
 }
 
 let apiKey = "290724cd93ad94b31t54c30cca2o800f";
-let context ="Provide specific information";
-let prompt = "When was AI first created?";
+let context ="Provide specific information, such as name, birth location and year";
+let prompt = "Who was the first female to develop AI?";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 console.log("processing");
