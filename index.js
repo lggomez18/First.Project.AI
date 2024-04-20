75660971fdd49707cedbb5e5d877aa49ba096360
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-04-20 12:02:25 -0500

    Created a new project index.js
    A new project to learn how to embed AI using APIs.

---
+++ index.js
@@ -0,0 +1,3 @@
+let apiKey = "290724cd93ad94b31t54c30cca2o800f";
+let context ="Provide specific information";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
