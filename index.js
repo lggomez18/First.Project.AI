48f623bac96c84c6dcd6c392de84bc1bf9d6b0b2
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-04-21 17:09:11 -0500

    javascrip in progress
    Applying a function to the button. When clicked, it will provide a different joke every time.

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-//function showAnswer(response) {
-    //alert(response.data.answer);
+function showAnswer(response) {
 
-//}
+  let buttonElement = document.querySelector("#jokeButton");
+  buttonElement.addEventListener("click", showAnswer);
+    
+  //alert(response.data.answer);
 
-//let apiKey = "290724cd93ad94b31t54c30cca2o800f";
-//let context ="Provide specific information, such as name, birth location and year";
-//let prompt = "Who was the first female to develop AI?";
-//let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-//console.log("processing");
-//axios.get(apiUrl).then(showAnswer);
+}
+
+let apiKey = "290724cd93ad94b31t54c30cca2o800f";
+let context ="Jokes about pets";
+let prompt = "Tell me one different short joke about cats";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log("processing");
+axios.get(apiUrl).then(showAnswer);
 
 
 
