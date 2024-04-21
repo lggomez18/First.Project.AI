0e1110114a70af59402c3d88e29b52d1d91bc63e
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-04-21 18:14:59 -0500

    function in progress
    Part of the function has been fixed- apiKey, context, prompt, and apiUrl; along with the axios that communicates with the function show joke.

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
-function showAnswer(response) {  
-let jokeSentence= document.querySelector ("#jokeSetence");
-jokeSentence.innerHTML= alert (response.data.answer);
-}
+function showJoke(response) {  
+alert (response.data.answer);
+
 
 let apiKey = "290724cd93ad94b31t54c30cca2o800f";
 let context ="You are a creative AI assistant that invents jokes about pets";
 let prompt = "Generate a short joke about pets";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-axios.get(apiUrl).then(showAnswer);
-
-
-
-
-function handleClick(){
-  let jokeSentenceElement= document.querySelector ("#jokeSetence");
-  jokeSentenceElement.addEventListener("click", handleClick);
-  jokeSentenceElement.innerHTML= alert (response.data.answer);
-
+axios.get(apiUrl).then(showJoke);
 }
 
-//let buttonElement = document.querySelector("#special-button");
-//buttonElement.addEventListener("click", handleClick);
 
-//let challenge2Element = document.querySelector("#challenge-2");
-//challenge2Element.innerHTML = "Matt Delac";
+function tellJoke (event){
+  
+}
 
-  //let buttonElement = document.querySelector("#jokeButton");
-  //buttonElement.addEventListener("click", showAnswer);
+
+  let buttonElement= document.querySelector ("#jokeButton");
+  buttonElement.addEventListener("click", tellJoke);
+
 
   //typerwritter
 
