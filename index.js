83c8520afd50d1a1c2119fa5b67fbe1ea91e05f6
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-04-25 20:34:45 -0500

    Typewriter + code
    Added notes for myself to better understand the code.

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 function generatePoem (event){
   event.preventDefault();
 
-//let poemElement = document.querySelector ("#poem");
 
-new Typewriter('#poem', {  // the #poem indicates where is the typewriter going to appear, document.querySelector basically
-  strings: ["La tombe dit..."], // it's de inner HTML part that will show in your page.
+new Typewriter('#poem', { 
+  //let poemElement = document.querySelector ("#poem");
+  strings: ["La tombe dit..."],
+  //poemElement.innerHTML = "La tombe dit...";
   autoStart: true,
   delay:1,
   cursor:"",
 });
 
-//poemElement.innerHTML = "La tombe dit...";
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
