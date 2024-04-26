9af625f1e8d32b8cf78c7f822312d9e5c681fc48
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-04-25 20:00:01 -0500

    Javascript in process

--- index.js
+++ index.js
@@ -1,43 +1,9 @@
 
-function showJoke (response){
-console.log(response.data.answer);
+function generatePoem (event){
+  event.preventDefault();
 
-new Typewriter("#joke", {
-  strings: response.data.answer,
-  autoStart: true,
-  cursor:"",
-  delay:15,
-});
+  alert("Generating poem");
 
 }
-
-
-
-function tellJoke(event) {  
-
-
-
-let apiKey = "290724cd93ad94b31t54c30cca2o800f";
-let context ="You are a creative AI assistant that invents jokes about pets";
-let prompt = "Generate a short joke about pets";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-axios.get(apiUrl).then(showJoke);
-}
-
-
-
-
-
-  let buttonElement= document.querySelector ("#jokeButton");
-  buttonElement.addEventListener("click", tellJoke);
-
-
-  //typerwritter
-
-//new Typewriter('h2', {
-  //strings: 'Matt Delac',
-  //autoStart: true,
-  //cursor:"",
-  //delay:10,
-//});
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener ("submit", generatePoem);
