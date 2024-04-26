046e83068989fc4dcb5a281b3d46313665c4bcfc
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-04-25 20:57:14 -0500

    Function using APIs in progress
    Developed functions using APIs, and creating a new function to display the code.

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
+function displayPoem(response){
+  new Typewriter('#poem', { 
+    //let poemElement = document.querySelector ("#poem");
+    strings: ["La tombe dit..."],
+    //poemElement.innerHTML = "La tombe dit...";
+    autoStart: true,
+    delay:1,
+    cursor:"",
+  });
+}
+
 
 function generatePoem (event){
   event.preventDefault();
 
+  //1- build the API url
+  let apiKey="290724cd93ad94b31t54c30cca2o800f";
 
-new Typewriter('#poem', { 
-  //let poemElement = document.querySelector ("#poem");
-  strings: ["La tombe dit..."],
-  //poemElement.innerHTML = "La tombe dit...";
-  autoStart: true,
-  delay:1,
-  cursor:"",
-});
+  //3 Write the prompt & context
+  let prompt="";
+  let context="";
+
+  //2- make a call to the API using axios
+  let apiURL = "https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}";
+  
+  axios.get(apiURL).then(displayPoem);
+  //4- display the generator poem by creating a function called 'displayPoem'.
+  //5- cut & paste typewriter code inside the function displayPoem
+
 
 }
 
