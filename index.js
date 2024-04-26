0d108347f536e0811a70578d8d3d91af0422faf5
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-04-25 20:09:26 -0500

    Added typewiter code to js

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 function generatePoem (event){
   event.preventDefault();
 
-let poemElement = document.querySelector ("#poem");
-poemElement.innerHTML = "La tombe dit...";
+//let poemElement = document.querySelector ("#poem");
+
+new Typewriter('#poem', {  // the #poem indicates where is the typewriter going to appear, document.querySelector basically
+  strings: ["La tombe dit..."], // it's de inner HTML part that will show in your page.
+  autoStart: true,
+});
+
+//poemElement.innerHTML = "La tombe dit...";
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
